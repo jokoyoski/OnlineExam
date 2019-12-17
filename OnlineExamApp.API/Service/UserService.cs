af80cb8f5bde506c0cf08e7bad4d1b28bced6238
Bomanaziba <bomana.ogoni@gmail.com>
2019-12-17 11:24:27 +0100

    modifying charts

--- OnlineExamApp.API/Service/UserService.cs
+++ OnlineExamApp.API/Service/UserService.cs
@@ -20,29 +20,41 @@ namespace OnlineExamApp.API.Service
         {
 
             if (userId <= 0) throw new ArgumentNullException(nameof(userId));
-            
+
             if (categoryId <= 0) throw new ArgumentNullException(nameof(categoryId));
+
+            decimal averageScoreUser = 0;
+            decimal averageScoreOverall = 0;
 
             //get all the user score for a specific category
             IEnumerable<IUserScore> usersScoreCollectionByUserIdAndCategoryId = await this._userScoreRepository.GetUserScoresByUserIdAndCategoryId(userId, categoryId);
-            //getting just the score value from the above list
-            decimal[] scoreUser = usersScoreCollectionByUserIdAndCategoryId.ToList().Select(p=>p.Score).ToArray();
-            //get the average user score from extension method
-            decimal averageScoreUser = scoreUser.Average();
+
+            if (usersScoreCollectionByUserIdAndCategoryId != null)
+            {
+                //getting just the score value from the above list
+                decimal[] scoreUser = usersScoreCollectionByUserIdAndCategoryId.ToList().Select(p => p.Score).ToArray();
+                //get the average user score from extension method
+                averageScoreUser = scoreUser.Average();
+            }
 
             //get all the scores for a category
             IEnumerable<IUserScore> usersScoreCollectionByCategoryId = await this._userScoreRepository.GetUserScoresByCategoryId(categoryId);
-            //getting just the score value from the above list
-            decimal[] scoreOverall = usersScoreCollectionByCategoryId.ToList().Select(p=>p.Score).ToArray();
-            //get the average user score from extension method
-            decimal averageScoreOverall = scoreOverall.Average();
+
+            if (usersScoreCollectionByCategoryId != null)
+            {
+                //getting just the score value from the above list
+                decimal[] scoreOverall = usersScoreCollectionByCategoryId.ToList().Select(p => p.Score).ToArray();
+                //get the average user score from extension method
+                averageScoreOverall = scoreOverall.Average();
+            }
+
 
 
             IPerformanceDisplayDto performanceDisplayDto = new PerformanceDisplayDto();
 
             performanceDisplayDto.UserAverageScore = new Chat();
             performanceDisplayDto.OverallAverageScore = new Chat();
-            
+
             //Passing user value
             performanceDisplayDto.UserAverageScore.Label = "User Performance";
             performanceDisplayDto.UserAverageScore.Data = new decimal[1];
