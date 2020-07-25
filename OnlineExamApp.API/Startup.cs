052d29fa56e926393cb625ad3dc8d38c2374bac6
Bomanaziba Ogoni <bomana.ogoni@gmail.com>
2020-07-25 11:47:24 +0100

    uncommented Fallback from startup.cs

--- OnlineExamApp.API/Startup.cs
+++ OnlineExamApp.API/Startup.cs
@@ -144,7 +144,7 @@ namespace OnlineExamApp.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                //endpoints.MapFallbackToController("Index","Fallback");
+                endpoints.MapFallbackToController("Index","Fallback");
             });
         }
     }
